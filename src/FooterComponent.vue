81dccbc31cfacda29d5ce6882c2329b4b818c9ed
RizkyNurMuzhaki22 <rizki.nurmuzhaki@gmail.com>
2023-07-27 14:34:40 +0700

    Belajar Windtail.CSS

--- src/FooterComponent.vue
+++ src/FooterComponent.vue
@@ -1,13 +1,13 @@
 <template>
-    <hr>
+    hr
     <div>
         <footer class="p-4 bg-white sm:p-6 dark:bg-gray-800">
     <div class="mx-auto max-w-screen-xl">
         <div class="md:flex md:justify-between">
             <div class="mb-6 md:mb-0">
                 <a href="https://flowbite.com" class="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-8" alt="FlowBite Logo" />
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                    <img src="./components/logo-transparent-png.png" class="mr-3 h-8" alt="FlowBite Logo" />
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Ky.Domain</span>
                 </a>
             </div>
             <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
@@ -15,7 +15,7 @@
                     <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
                     <ul class="text-gray-600 dark:text-gray-400">
                         <li class="mb-4">
-                            <a href="./components/logo-transparent-png.png" class="hover:underline">Ky.Domain</a>
+                            <a href="https://flowbite.com" class="hover:underline">Flowbite</a>
                         </li>
                         <li>
                             <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
